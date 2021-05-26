d7664f4ccd83a2f1c3d109740ad4260c04282a4c
DESKTOP-JCKFMQ8\82102 <pyy4126@gmail.com>
2021-05-26 11:26:55 +0900

    profile페이지

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(ps -ef | ${PROJECT_NAME} | grep jar | awk '{print $2}')
+CURRENT_PID=$(ps -ef | grep ${PROJECT_NAME} | grep jar | awk '{print $2}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
